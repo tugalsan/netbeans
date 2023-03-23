9ea708567bd73a336734c8c0dd351c0a630fb9b4
tugalsan karabacak <tugalsan@gmail.com>
2023-03-23 17:20:44 +0300

    bat files edit for windows

---
+++ build.bat
@@ -0,0 +1,7 @@
+cd netbeans-master
+set PATH=C:\me\codes\ide\netbeans\netbeans\extide\ant\bin;%PATH%
+set PATH=C:\java17\bin;%PATH%
+set JAVA_HOME=C:\java17
+
+ant build
+pause
---
+++ run.bat
@@ -0,0 +1,4 @@
+cd netbeans-master
+set PATH=C:\me\codes\ide\netbeans\netbeans\extide\ant\bin;%PATH%
+ant tryme
+pause
